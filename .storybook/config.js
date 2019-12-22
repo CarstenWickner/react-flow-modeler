84538e6bee69e106b6df5a0680799b3e63a5fdce
Carsten Wickner <Carsten.Wickner@gmx.de>
2019-12-22 01:20:46 +0100

    Initial setup

---
+++ .storybook/config.js
@@ -0,0 +1,19 @@
+import { addParameters, configure } from '@storybook/react';
+import { DocsPage, DocsContainer } from '@storybook/addon-docs/blocks';
+
+addParameters({
+    options: {
+        name: "Flow Modeler",
+        url: "https://github.com/CarstenWickner/react-flow-modeler",
+        showAddonPanel: false,
+        showSearchBox: false,
+        // sort sub-folders to the bottom alphabetically – but leave the individual stories in their order of declaration
+        storySort: (a, b) => a[1].id.substring(0, a[1].id.indexOf("--")).localeCompare(b[1].id.substring(0, b[1].id.indexOf("--")))
+    },
+    docs: {
+      container: DocsContainer,
+      page: DocsPage,
+    }
+});
+
+configure(require.context('../stories', true, /\.stories\.mdx$/), module);
